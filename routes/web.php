ca367d2e439c942960bfd994e9eb3826b5f52ff1
Febriansyah <febriansyahid@outlook.com>
2023-03-27 17:00:39 +0700

    add icon & logo

--- routes/web.php
+++ routes/web.php
@@ -32,4 +32,7 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
+    Route::get('/post', function () {
+        return Inertia::render('Post');
+    })->name('post');
 });
